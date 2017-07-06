68f1b8dd46d0b16ee3892e9a857b22c71a12b537
psxda97 <adarsh.ariyallur@gmail.com>
2017-07-06 17:12:28 +0530

    Slim7

--- slim.mk
+++ slim.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/thea/full_thea.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slimm stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := thea
-PRODUCT_NAME := lineage_thea
+PRODUCT_NAME := slim_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
